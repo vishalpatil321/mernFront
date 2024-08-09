776cbbf87e1767a4969ebc58231de8df0976e3e7
Vishal Dhone <150796650+vishalpatil321@users.noreply.github.com>
2024-08-09 15:17:37 +0530

    Update PrivateRoutes.js

--- src/PrivateRoutes.js
+++ src/PrivateRoutes.js
@@ -2,6 +2,6 @@ import {Navigate,Outlet} from 'react-router-dom';
 const PrivateRoutes = () => {
     const user = localStorage.getItem('news-user');
 
-    return user ? <Outlet/> : <Navigate to='/register'/>;
+    return user ? <Outlet/> : <Navigate to='/'/>;
 }
 export default PrivateRoutes;
